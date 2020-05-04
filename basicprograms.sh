ad1a95035b8b84a5837f6ee5649d39f8d88da563
softlider <webmaster@softlider.com.br>
2020-05-04 00:32:17 -0300

    Corrected basic programs

--- basicprograms.sh
+++ basicprograms.sh
@@ -3,6 +3,9 @@
 echo "Instalando programas básicos"
 echo "Os seguinte programas serão instalandos: unrar, gimp, vlc, inkscape, curl, filezilla, gdebi, gparted, ssh, audacity, net-tools"
 sudo apt install -y unrar gimp vlc inkscape curl filezilla gdebi gparted git ssh audacity net-tools 
+echo "Configurando o GIT"
+sudo git config --global user.name "softlider"
+sudo git config --global user.email "webmaster@softlider.com.br"
 echo "Corrigindo possíveis problemas"
 sudo apt install -f -y
 echo "Executando limpeza de sistema"
